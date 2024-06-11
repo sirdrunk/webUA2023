f8a217cddc606de3d50834a993f1b17588902e34
LastOfDevs <zaskita.traska@gmail.com>
2024-06-11 18:51:50 +0200

    Modificación de clave.php para que tuviera el mismo formato de página que el resto de la nueva web. Se agregó la dependencia analytics.php, en el fichero original de clave.php se instanciaba.

---
+++ analytics.php
@@ -0,0 +1,17 @@
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-592757-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+<script data-cfasync="false" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.js"></script>
+<!--<script data-cfasync="false" type="text/javascript" src="js/snow.min.js"></script>
+<script data-cfasync="false">
+$(document).ready( function(){
+    $.fn.snow();
+});
+</script>-->
--- clave.php
+++ clave.php
@@ -1,87 +1,131 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-<link rel="icon" type="image/png" href="favicon.png" />
-<title>Recuperar Clave - Ultima Alianza - Servidor español de Ultima Online</title>
+	<meta charset="utf-8">
+	<meta content="width=device-width, initial-scale=1.0" name="viewport">
+  
+	<!-- Specific for each page -->
+	<title>Recuperar Clave - Ultima Alianza (UA)</title>
+	<meta content="Servidor español de Ultima Online (UO), ¡Bienvenido a Ultima Alianza! (UA)" name="description">
+	<meta content="mmorpg,rol,ultima,online,uo,ua,juego,servidor,free,gratuito,privado,sphere,sphereserver,mago,caballero,paladin,nigromante,skills,stats,pvp,pvm,quests,misiones,recompensas,british,osi,uoassist,razor,easyuo,uoam,runuo,uofiddler,server,spanish,centred,centred+" name="keywords">
+	<!-- End Specific for each page -->
+  
+	<meta http-equiv="content-type" content="text/html; utf-8" />
+	<meta http-equiv="content-language" content="es" />
+	<meta name="distribution" content="global" />
+	<meta name="revisit-after" content="14 days" />
+	<meta name="copyright" content="Ultima Alianza" />
+	<meta name="robots" content="NOODP">
+	<meta name="robots" content="FOLLOW,INDEX" />
+  
+	<meta property="og:title" content="Ultima Alianza - Servidor español de Ultima Online" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://ultima-alianza.com" />
+	<meta property="og:description" content="Servidor español de Ultima Online, ¡Bienvenido a Ultima Alianza!" />
+	<meta property="og:site_name" content="Ultima Alianza" />
+	<meta property="og:image" content="https://ultima-alianza.com/images/logo.png" />
+  
+  
+	<meta name="twitter:card" content="Ultima Alianza - Servidor español de Ultima Online" />
+	<meta name="twitter:description" content="Servidor español de Ultima Online, ¡Bienvenido a Ultima Alianza!" />
+	<meta name="twitter:site" content="@uasphere" />
+	<meta name="twitter:title" content="Ultima Alianza" />
+	<meta name="twitter:url" content="https://ultima-alianza.com" />
+  
+	<!-- Favicons -->
+	<link href="assets/img/logo/ua.png" rel="icon">
+  
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  
+	<!-- Vendor CSS Files -->
+	<link href="assets/vendor/aos/aos.css" rel="stylesheet">
+	<link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+	<link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+	<link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+	<link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+	<link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  
+	<!-- Template Main CSS File -->
+	<link href="assets/css/style.css" rel="stylesheet">
+  
+	<!-- =======================================================
+	* Template Name: Gp - v4.10.0
+	* Template URL: https://bootstrapmade.com/gp-free-multipurpose-html-bootstrap-template/
+	* Author: BootstrapMade.com
+	* License: https://bootstrapmade.com/license/
+	======================================================== -->
+  
+	<script type="259ecbfce34d0135505f60e2-text/javascript">
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+	
+	  ga('create', 'UA-592757-1', 'auto');
+	  ga('send', 'pageview');
+	</script>
+  
+	<script src="assets/js/components.js"></script>
 
-<meta name="keywords" content="mmorpg,rol,ultima,online,uo,ua,juego,servidor,free,gratuito,privado,sphere,sphereserver,mago,caballero,paladin,nigromante,skills,stats,pvp,pvm,quests,misiones,recompensas,british,osi,uoassist,razor,easyuo,uoam,runuo,uofiddler,server,spanish"/>
-<meta name="description" content="Servidor español de Ultima Online (UO), ¡Bienvenido a Ultima Alianza!"/>
-<meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-<meta http-equiv="content-language" content="es"/>
-<meta name="distribution" content="global"/>
-<meta name="revisit-after" content="14 days"/>
-<meta name="copyright" content="Ultima Alianza"/>
-<meta name="robots" content="FOLLOW,INDEX"/>
-<!-- Begin Metadata Facebook -->
-<meta property="og:title" content="Ultima Alianza - Servidor español de Ultima Online" />
-<meta property="og:type" content="website" />
-<meta property="og:url" content="https://ultima-alianza.com" />
-<meta property="og:description" content="Servidor español de Ultima Online, ¡Bienvenido a Ultima Alianza!" />
-<meta property="og:site_name" content="Ultima Alianza"/>
-<meta property="og:image" content="https://ultima-alianza.com/images/logo.png" />
-<!-- End Metadata Facebook -->
-<!-- Begin Metadata Twitter -->
-<meta name="twitter:card" content="Ultima Alianza - Servidor de Ultima Online" />
-<meta name="twitter:description" content="Servidor español de Ultima Online, ¡Bienvenido a Ultima Alianza!" />
-<meta name="twitter:site" content="@uasphere" />
-<meta name="twitter:title" content="Ultima Alianza" />
-<meta name="twitter:url" content="https://ultima-alianza.com" />
-<!-- End Metadata Twitter -->
-
-<link rel="stylesheet" type="text/css" href="style.css" />
-<link rel="image_src" type="image/jpeg" href="//ultima-alianza.com/images/logo.jpg" />
-
-<style type="text/css">
-<!--
-.Estilo11 {
-	color: #000000;
-	font-size: 14;
-	font-weight: bold;
-}
-.Estilo12 {
-	font-size: 18px;
-	font-weight: bold;
-	color: #FF6600;
-}
-.Estilo13 {color: #000000}
--->
-</style>
 </head>
 
-<body class="details">
-<?php include_once("analytics.php") ?>
-<div class="wrap">
-	<div class="header">
-	  <div class="logo"><a href="/"><img src="images/logo.png" alt="" title="" border="0" /></a></div>
-        <div id="menu">
-            <ul>
-            <li><a href="normas.html">Normas</a></li>
-			<li><a href="jugar.html">Jugar</a></li>
-            <li><a href="elservidor.html">El Servidor</a></li>
-            <li><a href="status.php">Estado</a></li>
-            <li><a href="donacion.html">Power UP</a></li>
-            <li><a href="contacta.html">Contacta</a></li>
-            </ul>
+<body>
+  <?php include_once("analytics.php") ?>
+  <!-- ======= Header ======= -->
+  <ua-header current="recuperar" inner="true"></ua-header><!-- End Header -->
+
+  <main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section class="breadcrumbs">
+      <div class="container">
+        <div class="d-flex justify-content-between align-items-center">
+          <h2 class="pt-3">Recuperación de Contraseña</h2>
+          <ol>
+            <li><a href="index.html">Inicio</a></li>
+            <li>Recuperación de Contraseña</li>
+          </ol>
         </div>
-		<div class="horarios">
-			<iframe src="horario_torneos.php" frameborder="0" allowtransparency="true" class="iframe_horarios"></iframe>
-		</div>
-  </div>
-   <div class="main_content">
-		<div class="wide_content">
-			<div class="text_main_content">	
-			</br></br>
-				<center>
-					<h3>Recuperación de contraseña</h3>
-					<?php include("C://UA//Servidor//Sphere//Web//cuenta//clave.php"); ?>
-				</center>
-			</div>
-			</div>
-		<div class="clear"></div>
-  </div>
-<br><br>
-<div class="big_title"><div align="right">Síguenos <img src="images/separador.png" align="right"/><a href="http://www.youtube.com/user/UASphereStaff"><img src="images/youtube.png" align="right" alt="Youtube" title="Youtube" /></a><img src="images/separador.png" align="right"/><a href="http://www.facebook.com/pages/UASphere/113901315347603"><img src="images/fb.png" align="right" alt="Fb" title="Fb" /></a><img src="images/separador.png" align="right"/><a href="http://www.irc-hispano.es/webchat/webchat.php?canal=ua-sphere&locked=1"><img src="images/irc.png" align="right"alt="Chat IRC" title="Chat IRC" /></a><img src="images/separador.png" align="right"/><a href="http://twitter.com/uasphere"><img src="images/twit.png" align="right"alt="Twit" title="Twit" /></a><img src="images/separador.png" align="right"/></div></div>
-<br><br><br><br>
-</div>
+      </div>
+    </section><!-- End Breadcrumbs -->
+
+    <section class="inner-page">
+      <div class="container">
+          <div id="recupera_pass" class="row">
+            <div class="col">
+              <p><img src="assets/img/candado_completo.jpg" /></p>
+            </div>
+            <div class="col">
+              <h2 class="pt-3">Recuperación de Contraseña</h2>
+			  <?php include("C://UA//Servidor//Sphere//Web//cuenta//clave.php"); ?>
+            </div>
+          </div>
+        
+        
+        </div>
+      </div>
+    </section>
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <ua-footer></ua-footer>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
 </body>
+
 </html>
