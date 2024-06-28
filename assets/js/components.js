3dc9b5d4c7454a5b17077cfa051ffddc73750d66
LastOfDevs <zaskita.traska@gmail.com>
2024-06-28 21:42:00 +0200

    #10 Agregados los enlaces a Recuperar Cuenta desde el Menú

--- assets/js/components.js
+++ assets/js/components.js
@@ -123,7 +123,7 @@ class UaHeader extends HTMLElement{
                   <li><a href="https://ultima-alianza.com/foro/">Foro</a></li>
                   <li><a href="changelog/index.html">Changelog</a></li>
                   <li><a href="clave.html">Recuperar Contraseña</a></li>
-                  <li><a href="">Recuperar Cuenta</a></li>
+                  <li><a href="recuperar_cuenta.html">Recuperar Cuenta</a></li>
                   <li><a href="index.html#team">Staff</a></li>
                 </ul>
               </li>
